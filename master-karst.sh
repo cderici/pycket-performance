295c3d4965e3b6b85dabcd245348c0a2e2f71309
Caner Derici <canerderici@gmail.com>
2019-02-12 21:00:04 -0500

    deterministically decide if the jobs are done before continuing, otherwise wait

--- master-karst.sh
+++ master-karst.sh
@@ -7,9 +7,12 @@
   
 cd "$PBS_O_WORKDIR"
 
+echo "Subject: master-karst.sh is done."
+
 cd ./control-room
 make clean
 echo > ../experiment-status
+echo "cleaning done."
 if [ "${WHAT}" = "traces" ]; then
     rm -rf ../timings-traces
     mkdir ../timings-traces
@@ -21,17 +24,54 @@ else
     mkdir ../timings-pycket
     make pycket-new
 fi
+echo "scripts are generated, starting to run ..."
 make run
-sleep 30m
+# wait for the new* jobs to finish
+while true; do
+    wait=0
+    while read -r line; do
+	if [[ $line == *"new-"* ]]; then
+	    wait=1
+	    break
+	fi
+    done < <(qstat -u cderici)
+    if [[ $wait -eq 0 ]]; then
+	echo "new-* jobs are done, start the old-* tests"
+	break
+    else
+	echo "waiting... `date '+%Y-%m-%d %H:%M:%S'`"
+	sleep 10m
+    fi
+done
+
 make clean-scripts
 if [ "${WHAT}" = "traces" ]; then
     make old-traces
 else
     make pycket-old
 fi
+echo "scripts are generated, starting to run"
 make run
-sleep 30m
+# wait for the new* jobs to finish
+while true; do
+    wait=0
+    while read -r line; do
+	if [[ $line == *"old-"* ]]; then
+	    wait=1
+	    break
+	fi
+    done < <(qstat -u cderici)
+    if [[ $wait -eq 0 ]]; then
+	echo "old-* jobs are done, start the analysis"
+	break
+    else
+	echo "waiting... `date '+%Y-%m-%d %H:%M:%S'`"
+	sleep 10m
+    fi
+done
+echo "cleaning and exiting"
 make clean-scripts
 cd ..
+echo "analysis starts"
 racket analyze-rebench-output.rkt ${WHAT}
 cp experiment-status tmp/experiment-status_`date '+%Y-%m-%d_%H:%M:%S'`
