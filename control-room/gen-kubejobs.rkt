d97c056dd0e2ff6382cdbbf818f921a7bc89ec76
Caner Derici <caner@dericilab.live>
2024-10-26 00:35:31 +0000

    refactor(k8s-gen): cleanup

--- control-room/gen-kubejobs.rkt
+++ control-room/gen-kubejobs.rkt
@@ -2,48 +2,49 @@
 
 (provide benchmarks)
 
-#;(define benchmarks (list 'ack ;-- ready -- OK
-		         'array1 ;-- ready -- OK
-                         'cpstak ;-- ready -- OK
-                     	 'ctak ;-- ready -- OK
-                     	 'diviter ;-- ready -- OK
-                     'divrec ;-- ready -- OK
-                     'fft ;-- read -- OK
-                     ;'fibc ;-- ready -- OK
-                     'fibfp ;-- ready -- OK
-                     'fib ;-- ready -- OK
-                     'gcbench ;-- ready -- OK -------------------------------
-                     'mbrot ;-- ready -- OK
-                     'nqueens ;-- ready -- OK
-                     'nucleic ;-- ready -- OK
-                     'paraffins ;-- ready -- OK
-                     'pi ;-- ready -- OK
-                     'pnpoly ;-- ready -- OK
-                     'string ;-- ready -- OK
-                     'sumfp ;-- ready -- OK
-                     'sumloop ;-- ready -- OK
-                     ;'sumrecfp ;-- ready -- Ok
-                     'sumrec ;-- ready -- OK
-                     'sum ;-- ready -- OK
-                     'takl ;-- ready -- OK
-                     'tak ;-- ready -- OK
-                     'perm9 ;-- ready -- OK
-                     'gcold ;-- ready -- OK
-                     'deriv ;-- ready -- OK
-                     'primes ;-- ready -- OK
-                     'ray ;-- ready -- OK  ------------------------------------
-                     'puzzle ;-- ready -- OK
-                     'triangl ;-- ready -- OK
-                     ; 'sum1 ;-- ready -- OK ------------------------
-                     'wc ;-- ready -- OK -----------------------------
-                     'cat ;-- ready -- OK ----------------------------
-                     'graphs ;-- ready -- OK
-                     'tail ;-- ready -- OK ----------------------------
-                     'nboyer ;-- ready -- OK
-                     'earley ;-- ready -- OK
-                     'sboyer ;-- ready -- OK
-                     'mazefun ;-- ready -- OK
-                     'simplex ;-- ready -- OK
+#;(define benchmarks (list
+                      'ack
+		                  'array1
+                      'cpstak
+                      'cta
+                      'diviter
+                      'divrec
+                      'ff
+                      ;'fibc
+                      'fibfp
+                      'fib
+                      'gcbench
+                      'mbrot
+                      'nqueens
+                      'nucleic
+                      'paraffins
+                      'pi
+                      'pnpoly
+                      'string
+                      'sumfp
+                      'sumloop
+                      ;'sumrecfp
+                      'sumrec
+                      'sum
+                      'takl
+                      'tak
+                      'perm9
+                      'gcold
+                      'deriv
+                      'primes
+                      'ray
+                      'puzzle
+                      'triangl
+                      ; 'sum1
+                      'wc
+                      'cat
+                      'graphs
+                      'tail
+                      'nboyer
+                      'earley
+                      'sboyer
+                      'mazefun
+                      'simplex
                      ))
 
 (define benchmarks (list
@@ -166,165 +167,6 @@ done\n\n
                 (launch-function bench-name old/new with/no-warmup generate-traces?)
                 (log-line old/new sys bench-name with/no-warmup generate-traces? "COMPLETED"))))))
 
-(define script-template
-  "#!/bin/bash
-
-NFS_SHARE=/mnt/nfs_share
-BENCH_DIR=$NFS_SHARE/benchmarks
-PYCKET_DIR=/opt/pycket
-SOURCE_DIR=$NFS_SHARE/pycket-performance/src
-OUTPUT_DIR=$BENCH_DIR/timings-pycket
-
-
-echo \"new pycket ack #t-warmup - `date '+%Y-%m-%d %H:%M:%S'` - STARTED on pod: $POD_NAME\" >> $BENCH_DIR/experiment-status
-
-$PYCKET_DIR/pycket-c-linklets $SOURCE_DIR/with-warmup/ack.rkt &>> $OUTPUT_DIR/new-pycket-ack-with-warmup.rst
-
-echo \"new pycket ack #t-warmup - `date '+%Y-%m-%d %H:%M:%S'` - COMPLETED on pod: $POD_NAME\" >> $BENCH_DIR/experiment-status")
-
-
-#;(define racket-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"~a racket START\" >> ~~/benchmark-status
-
-racket ~a-nothing.rkt &>> ~~/timings-racket/~a-racket.rst
-
-echo \"~a racket DONE\" >> ~~/benchmark-status ")
-
-#;(define (gen-racket bench)
-  (format racket-template bench bench bench bench))
-
-;; RACKETS
-
-#|
-(for ([bench benchmarks])
-  (call-with-output-file (format "~a-racket.sh" bench)
-    (λ (op) (display (gen-racket bench) op))
-    #:exists 'replace))
-
-;; batch
-(let ([file-name "run-racket.sh"]
-      [scripts-str (apply string-append (map (lambda (b) (format "qsub ~a-racket.sh\n" b)) benchmarks))])
-  (call-with-output-file file-name
-    (lambda (op) (display (string-append "#!/bin/bash \n\n" scripts-str) op))
-    #:exists 'replace))
-|#
-
-;~~/pycket/pycket-c~a ~a-nothing.rkt &> ~~/benchresults/~a-~a.rst
-
-;; INDIVIDUAL BENCHMARKS
-
-#;(for ([bench benchmarks])
-  (for ([mode MODES])
-    (call-with-output-file (format "~a-~a-~a.sh" bench RUN_NAME mode)
-      (λ (op) (display (gen-script bench mode) op))
-      #:exists 'replace)))
-
-;; BATCH RUNNERS
-#;(for ([mode MODES])
-  (let ([file-name (format "run-~a-~a.sh" RUN_NAME mode)]
-        [scripts-str (apply string-append (map (lambda (b) (format "qsub ~a-~a-~a.sh\n" b RUN_NAME mode)) benchmarks))])
-    (call-with-output-file file-name
-      (lambda (op) (display (string-append "#!/bin/bash \n\n" scripts-str) op))
-      #:exists 'replace)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; UNROLLING STUFF
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define benchs '(fib sum sumtail sumrec tak))
-
-(define unroll-script-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"START ===> ~a unroll : ~a - in-process : ~a times\" >> ~~/benchmark-status
-
-for i in `seq 1 20`;
-do
-	~~/pycket/pycket-c ~a-~a-~a-nothing.rkt &>> ~~/timings/unroll/~a-~a-~a.rst
-done
-
-echo \"DONE ===> ~a unroll : ~a - in-process : ~a times\" >> ~~/benchmark-status ")
-
-;(define master-script-name "run-unrolls.sh")
-;; one script to rule them all
-
-#;(call-with-output-file master-script-name
-  (lambda (op)
-    (display "#!/bin/bash \n\n" op)
-    (for ([b benchs])
-      (for ([unroll '(0 1 2 3 4 5 6)])
-        (for ([in-proc '(1 20)])
-          (let ([file-name (format "~a-~a-~a-nothing.rkt" b unroll in-proc)]
-                [bash-script-name (format "~a-~a-~a.sh" b unroll in-proc)])
-
-            ;; (1) write the <bench>-nothing.rkt
-            (call-with-output-file file-name
-              (lambda (op)
-                (display (format "(module ~a-~a-~a pycket/wrap nothing)" b unroll in-proc) op))
-              #:exists 'replace)
-
-            ;; (2) write the corresponding bash script
-            (call-with-output-file bash-script-name
-              (lambda (op)
-                (display (format unroll-script-template b unroll in-proc b unroll in-proc b unroll in-proc b unroll in-proc) op))
-              #:exists 'replace)
-
-            ;; (3) write the line to call the above script in the master script
-            (display (format "qsub ~a\n" bash-script-name) op)
-            )))))
-  #:exists 'replace)
-
-(define traces-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"START ===> ~a ~a ~a ~a-warmup traces - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status
-
-PYPYLOG=jit-log-opt,jit-backend,jit-summary:../traces/~a-~a.trace ~a ~a/~a.rkt &>> ../timings-traces/~a-~a-~a-traces.rst
-
-echo \"DONE ===> ~a ~a ~a ~a-warmup traces - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status "
-)
-
-(define with-warmup-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"START ===> ~a ~a ~a ~a-warmup - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status
-
-~a ~a/~a.rkt &>> ../timings-pycket/~a-~a-~a-with-warmup.rst
-
-echo \"DONE ===> ~a ~a ~a ~a-warmup - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status "
-)
-
-(define racket-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"START ===> ~a ~a ~a - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status
-
-~a ~a/~a.rkt &>> ../timings-racket/~a-~a-~a.rst
-
-echo \"DONE ===> ~a ~a ~a - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status "
-)
-
-(define without-warmup-template
-  "#!/bin/bash
-
-cd \"$PBS_O_WORKDIR\"
-echo \"START ===> ~a ~a ~a ~a-warmup - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status
-
-for i in `seq 1 100`;
-do
-	~a ~a/~a.rkt &>> ../timings-pycket/~a-~a-~a-no-warmup.rst
-done
-
-echo \"DONE ===> ~a ~a ~a ~a-warmup - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experiment-status "
-)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;; GENERATORS FOR KUBERNETES JOBS ;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -357,28 +199,8 @@ echo \"DONE ===> ~a ~a ~a ~a-warmup - `date '+%Y-%m-%d %H:%M:%S'`\" >> ../experi
    #:args ()
    (printf "\nGENERATING ~a jobs\nMODE : ~a\nwith-warmup? : ~a\ngenerate-traces? : ~a\n\n" sys mode with-warmup? traces)
 
-   #;(when traces
-      (unless (equal? sys 'pycket)
-	 	    (error 'gen-scripts "system has to be pycket to generate JIT backend logs")))
-
-   #;(when (equal? sys 'racket) (set! with-warmup? #t))
-
    (for ([b (in-list benchmarks)])
-     (let* ([config (kubejob-config b sys mode with-warmup? traces)]
-            [bash-script-name (if (equal? sys 'racket)
-     	   		      	  (format "~a-~a-~a.sh" mode sys b)
-              (if traces
-                  (format "~a-~a-~a-traces.sh" mode sys b)
-                  (format "~a-~a-~a-~a-warmup.sh" mode sys b (if with-warmup? "with" "no"))))]
-            #;[command (if (equal? sys 'racket)
-                         (if (equal? mode 'old) (format "~~/racketland/racket/bin/racket")
-			     	       	     (format "~~/racketland/racket/racket/bin/racket"))
-                         (if (equal? mode 'new) "~/pycketland/pycket/pycket-c-linklets" "~/pycketland/pycket/pycket-c"))]
-            #;[src-dir (if traces
-	    	     	 "../src/for-traces"
-	    	         (if with-warmup?
-                             "../src/with-warmup"
-                             "../src/without-warmup"))])
+     (let ([config (kubejob-config b sys mode with-warmup? traces)])
         (let-values ([(script-path script-content) (gen-script config)])
           (call-with-output-file script-path
             (lambda (bop)
